f6d759b053f1844ed707bc9c29e0510834002b97
vansh0singhal <vanshsinghal101@gmail.com>
2025-09-17 00:11:33 +0530

    learn hoisting

---
+++ hoisting.js
@@ -0,0 +1,20 @@
+console.log(a);
+console.log(getName)
+helo();
+
+function helo(){
+    console.log("Hello world")
+}
+
+
+var a=10;
+var getName=()=> {
+    console.log("Vansh Singhal");
+}
+
+
+console.log(a);
+console.log(getName)
+helo();
+
+
