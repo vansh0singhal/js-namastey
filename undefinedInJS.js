3220276868d202c422f5ed4c834f5c1bafea8e4a
vansh0singhal <vanshsinghal101@gmail.com>
2025-09-17 18:35:51 +0530

    undefined

---
+++ undefinedInJS.js
@@ -0,0 +1,19 @@
+console.log(a);
+
+var a;
+
+if(a===undefined){
+    console.log("a is undefined");
+}else{
+    console.log("a is not defined");
+}
+
+
+
+//js is a looseley typed language
+var x;
+console.log(x);
+x=10;
+console.log(x);
+x="hello";
+console.log(x);
